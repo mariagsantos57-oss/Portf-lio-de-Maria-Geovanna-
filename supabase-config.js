51c6cfea95c1dfe18c5fceb0e8dd06de8d48fe1e
Maria Geovanna <maria.g.santos57@aluno.senai.br>
2026-09-21 17:20:02 -0300

    Configurar credenciais do Supabase

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = " https://etptbehghrmofoaxhghp.supabase.co/rest/v1/";
-const SUPABASE_PUBLISHABLE_KEY = " sb_publishable_vayhdKabenv4NTFP7ePP_g_cSe1aaZH";
+const SUPABASE_URL = "https://etptbehghrmofoaxhghp.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_vayhdKabenv4NTFP7ePP_g_cSe1aaZH";
 
 export const supabaseClient =
     window.supabase.createClient(
